cb7bedc63de9a1877e7268a19ad58c065936742c
Rafael Gomes <rafarfl2010@gmail.com>
2020-05-02 16:46:33 -0300

    Doing unit tests part 2

--- app1/index.js
+++ app1/index.js
@@ -1,7 +1,10 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const somarNumeros = (a,b) => a + b;
+const somarNumeros = (a,b) =>  {
+	if (typeof a === "number" && typeof b === "number") return  a + b;
+	else return undefined;
+}
 
 describe('Somas', () => {
 	it('Soma de dois numeros - 2 e 3', (done) => {
@@ -13,6 +16,18 @@ describe('Somas', () => {
 	it('Soma de dois numeros - -2 e 3', (done) => {
 		const resultado = somarNumeros(-2,3);
 		expect(resultado).be.equal(1);
+		done();
+	})
+	
+	it('Soma de dois numeros - "teste" e 3', (done) => {
+		const resultado = somarNumeros("teste",3);
+		expect(resultado).be.equal(undefined);
+		done();
+	})
+	
+	it('Soma de dois numeros - null e 3', (done) => {
+		const resultado = somarNumeros(null,3);
+		expect(resultado).be.equal(undefined);
 		done();
 	})
 })
